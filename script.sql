4c8bda74159111382bbb14fb96e95e0b3b015242
mhmdalkhyyt <mhmdalkhyyt@gmail.com>
2022-09-18 12:22:07 -0700

    direction change

--- script.sql
+++ script.sql
@@ -2,18 +2,21 @@ DROP DATABASE IF EXISTS a20muhal;
 CREATE DATABASE a20muhal;
 USE a20muhal;
 
-CREATE TABLE tomtenisse(
-	Namn varchar(255),
-    IdNr int(3),
-    Lon int(3),
-    Adress varchar(255),
-     
-    primary key (Namn)
-)ENGINE=INNODB; 
+CREATE TABLE Ren(
+    Nr int  unique not null,
+    Namn varchar(255),
+    Stank varchar(255),
+    Region varchar(255),
 
-CREATE TABLE verktyg(
-	Namn varchar(255),
-	
-    primary key (Namn)
-)ENGINE=INNODB; 
+    primary key (Nr)
 
+)ENGINE=INNODB;
+
+CREATE TABLE TjänstRen(
+    Lön float,
+    RenNr int,
+
+    primary key(RenNr),
+    foreign key(RenNr) REFERENCES Ren(Nr)
+
+)ENGINE=INNODB;
